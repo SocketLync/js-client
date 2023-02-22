1d823906831b836a44a3c665eb85e0bd9e483505
derpierre65 <git@derpierre65.de>
2023-02-22 20:54:09 +0000

    added channel export

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 export {default as default} from './SocketLync';
+export {default as Channel} from './channels/Channel';
+export {default as PrivateChannel} from './channels/PrivateChannel';
+export {default as PresenceChannel} from './channels/PresenceChannel';
